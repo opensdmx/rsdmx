994a876812fa27a9f669ffe9b20095edff00dea6
eblondel <emmanuel.blondel1@gmail.com>
2014-02-13 19:20:09 +0100

    SDMXheader modifications

--- R/Class-SDMXHeader.R
+++ R/Class-SDMXHeader.R
@@ -1,18 +1,105 @@
-# E.Blondel - 2013/06/09
-#=======================
+# rsdmx - RSDMXHeader class
+#
+# Author: Emmanuel Blondel
+#==========================
+
+#TODO for year (length=4) -> convert first to ISODate &then to POSIXlt/strptime
+# conversion to ISODates:
+# - Prepared: use 1st january
+# - reportingStart: use 1st january
+# - reportingYear: use 31th december
+#TODO for year-month-day -> usePOSIXlt/strptime (with format %Y-%m-%d)
+#TODO for dayTime --> usePOSIXlt/ check if T in the string, and apply appropriate format
 
 #SDMXHeader class
 setClass("SDMXHeader",
 		representation(
-			ID = "character",
-			Test = "logical",
-			Prepared = "POSIXt",
-			Sender = "character"
+			ID 	= "character", Test = "logical", Truncated = "logical",
+			Name = "character", Sender = "character", Receiver = "character",
+			Prepared = "POSIXlt",
+			#Extracted = "POSIXlt",
+			#ReportingBegin = "POSIXlt", ReportingEnd = "POSIXlt",
+			#KeyFamilyRef = "character", # xs:NMTOKEN?
+			#KeyFamilyAgency = "character", # xs:NMTOKEN?
+			#DataSetAgency = "character", # xs:NMTOKEN?
+			#DataSetID = "character",
+			#DataSetAction = "character", #TODO see ActionType
+			#
+			
+			Source = "character"
 		),
-		prototype = list(ID = "An ID", Test = FALSE, Prepared = Sys.time(), Sender = "A Sender ID"),
+		prototype = list(
+			ID = "Id", Test = FALSE, Truncated = FALSE,
+			Name = "name", Sender = "SenderID", Receiver = "ReceiverID",
+			Prepared = Sys.time(),
+			#Extracted = Sys.time(),
+			#ReportingBegin = Sys.time(), ReportingEnd = Sys.time(),
+			#KeyFamilyRef = "character", # xs:NMTOKEN
+			#KeyFamilyAgency = "character", # xs:NMTOKEN
+			#DataSetAgency = "character", # xs:NMTOKEN
+			#DataSetID = "datasetID",
+			#DataSetAction = "character", #TODO see ActionType
+			#Extracted = "POSIXt", #dateTime
+			
+			Source = "source"
+			),
 		validity = function(object){
 			
-			#eventual validation rules
+			# ID validation
+			if(is.null(object@ID)){ return(FALSE); }
+			if(attr(regexpr("[a-zA-Z0-9@-_@\\$]", object@ID),"match.length") == -1){ return(FALSE); }
+			
+			#Test/Truncated validation
+			#if(is.na(object@Test)){ return(FALSE); }
+			#if(is.na(object@Truncated)){ return(FALSE);}
+			
+			#Dates/Time validation
+			#if(is.null(object@Prepared) || is.na(object@Prepared)){ return(FALSE); }
+			
+			#Sender/Receiver validation
+			#if(is.na(object@Sender) || nchar(object@Sender) == 0){ return(FALSE);}
+			#if(nchar(object@Receiver) == 0){ return(FALSE);}
+			
+			#TODO KeyFamilyRef validation
+			#TODO KeyFamilyAgency validation
+			#TODO DataSetAgency validation
+			
+			#DatasetID validation
+			#if(object@DatasetID != NULL && length(object@Receiver) == 0) return(FALSE);
+			
+			#TODO DataSetAction validation
+			#TODO ReportingBegin validation
+			#TODO ReportingEnd validation
+			
 			return(TRUE);
 		}
 )
+
+#	<xs:sequence>
+#	<xs:element name="ID" type="common:IDType"/>
+#	<xs:element name="Test" type="xs:boolean" default="false"/>
+#	<xs:element name="Truncated" type="xs:boolean" minOccurs="0"/>
+#	<xs:element name="Name" type="common:TextType" minOccurs="0" maxOccurs="unbounded"/>
+#	<xs:element name="Prepared" type="HeaderTimeType"/>
+#	<xs:element name="Sender" type="PartyType" maxOccurs="unbounded">
+#	<xs:unique name="SDMX_SenderNameLanguageUniqueness">
+#	<xs:selector xpath="message:Name"/>
+#	<xs:field xpath="@xml:lang"/>
+#	</xs:unique>
+#	</xs:element>
+#	<xs:element name="Receiver" type="PartyType" minOccurs="0" maxOccurs="unbounded">
+#	<xs:unique name="SDMX_ReceiverNameLanguageUniqueness">
+#	<xs:selector xpath="message:Name"/>
+#	<xs:field xpath="@xml:lang"/>
+#	</xs:unique>
+#	</xs:element>
+#	<xs:element name="KeyFamilyRef" type="xs:NMTOKEN" minOccurs="0"/>
+#	<xs:element name="KeyFamilyAgency" type="xs:NMTOKEN" minOccurs="0"/>
+#	<xs:element name="DataSetAgency" type="xs:NMTOKEN" minOccurs="0"/>
+#	<xs:element name="DataSetID" type="xs:NMTOKEN" minOccurs="0"/>
+#	<xs:element name="DataSetAction" type="common:ActionType" minOccurs="0"/>			
+#	<xs:element name="Extracted" type="xs:dateTime" minOccurs="0"/>
+#	<xs:element name="ReportingBegin" type="HeaderTimeType" minOccurs="0"/>
+#	<xs:element name="ReportingEnd" type="HeaderTimeType" minOccurs="0"/>
+#	<xs:element name="Source" type="common:TextType" minOccurs="0" maxOccurs="unbounded"/>
+#	</xs:sequence>
--- R/SDMXHeader-methods.R
+++ R/SDMXHeader-methods.R
@@ -1,31 +1,102 @@
-# E.Blondel - 2013/06/09
-#=======================
+# rsdmx - SDMXHeader-methods
+#
+# Author: Emmanuel Blondel
+#==========================
 
 SDMXHeader <- function(xmlObj){
-	
-	#header values
+
+	#header elements
 	node <- getNodeSet(xmlObj, "//message:Header");
 	p <- as.data.frame(sapply(node, function(x) (sapply(xmlChildren(x), function(x) xmlValue(x)))));
 	p[,1] <- as(p[,1], "character");
 	
 	#header attributes
-	Sender <- xmlGetAttr(getNodeSet(xmlDoc(node[[1]]), "//message:Sender")[[1]],"id");
+	id <- p["ID",];
+	test <- as.logical(p["Test",]);
+	if(is.null(test)) test <- FALSE;
+	truncated <- as.logical(p["Truncated",]);
+	name <- p["Name",];
+
+	
+	sender <- "NA";
+	senderNode <- getNodeSet(xmlDoc(node[[1]]), "//message:Sender");
+	if(length(senderNode) > 0) sender <- xmlGetAttr(getNodeSet(xmlDoc(node[[1]]), "//message:Sender")[[1]],"id");
+	receiver <- "NA"
+	receiverNode <- getNodeSet(xmlDoc(node[[1]]), "//message:Receiver");
+	if(length(receiverNode) > 0) receiver <- xmlGetAttr(getNodeSet(xmlDoc(node[[1]]), "//message:Receiver")[[1]], "id");
+	
+	source <- p["Source",];
+	
+	#Dates
+	preparedFormat <- NULL;
+	prepared <- p["Prepared",];
+	if(!is.null(prepared)){
+		if(regexpr("T", prepared) != 1){
+			preparedFormat <- "%Y-%m-%d %H:%M:%S";
+		}else{
+			if(nchar(prepared) == 4){
+				prepared <- ISOdate(as.integer(prepared),1,1)
+				preparedFormat <- "%Y-%m-%d";
+			}else{
+				preparedFormat <- "%Y-%m-%dT%H:%M:%S";
+			}
+		}
+		prepared <- as.POSIXlt(strptime(prepared, format = preparedFormat));
+	}
+	#extractedFormat <- NULL;
+	#extracted <- p["Extracted",];
+	#if(!is.na(extracted)){
+	#	if(regexpr("T", extracted) != 1){
+	#		extractedFormat <- "%Y-%m-%d %H:%M:%S";
+	#	}else{
+	#		extractedFormat <- "%Y-%m-%dT%H:%M:%S";
+	#	}
+	#	extracted <- as.POSIXlt(strptime(extracted, format = extractedFormat));
+	#}
+		
+	#Reporting Dates
+	#beginFormat <- NULL;
+	#reportBegin = p["ReportingBegin",];
+	#if(!is.na(reportBegin)){
+	#	if(regexpr("T", reportBegin) != 1){
+	#		beginFormat <- "%Y-%m-%d %H:%M:%S";
+	#	}else{
+	#		if(nchar(reportBegin) == 4){
+	#			reportBegin <- ISOdate(as.integer(reportBegin),1,1)
+	#			beginFormat <- "%Y-%m-%d";
+	#		}else{
+	#			beginFormat <- "%Y-%m-%dT%H:%M:%S";
+	#		}
+	#	}
+	#}
+	#reportBegin <- as.POSIXlt(strptime( reportBegin, format = beginFormat));
+	
+	#endFormat <- NULL;
+	#reportEnd = p["ReportingEnd",];
+	#if(!is.na(reportEnd)){
+	#	if(regexpr("T", reportEnd) != 1){
+	#		endFormat <- "%Y-%m-%d %H:%M:%S";
+	#	}else{
+	#		if(nchar(reportEnd) == 4){
+	#			reportEnd <- ISOdate(as.integer(reportEnd),12,31)
+	#			endFormat <- "%Y-%m-%d";
+	#		}else{
+	#			endFormat <- "%Y-%m-%dT%H:%M:%S";
+	#		}
+	#	}
+	#}
+	#reportEnd <- as.POSIXlt(strptime( reportEnd, format = endFormat));
+	
 	
 	#SDMXHeader object
 	obj <- new("SDMXHeader",
-				ID = p["ID",],
-				Test = as.logical(p["Test",]),
-				Prepared = strptime(p["Prepared",], format = "%Y-%m-%dT%H:%M:%S"),
-				Sender = Sender);
-	
-	type <- SDMXType(xmlObj)@type;	
-	if(type == "SDMXDataSet"){
-		obj <- new("SDMXDataSetHeader",
-			obj,
-			ReportingBegin = p["ReportingBegin",],
-			ReportingEnd = p["ReportingEnd",]);
-	}
-	
+				ID = id, Test = test, Truncated = truncated,
+				Name = name, Sender = sender, Receiver = receiver,
+				Prepared = prepared,
+				#Extracted = extracted,
+				#ReportingBegin = reportBegin, ReportingEnd = reportEnd,
+				Source = source);
+
 	return(obj);
 }
 
