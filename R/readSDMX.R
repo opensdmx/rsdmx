535555a1319ecb11170b4a06fdb460d6c9965c18
Albrecht Gradmann <agradmann@gmail.com>
2014-07-16 22:44:03 +0200

    added Roxygen Documentation to readSDMX

--- R/readSDMX.R
+++ R/readSDMX.R
@@ -1,3 +1,31 @@
+#' @title readSDMX
+#'
+#' @description
+#' \code{readSDMX} is the main function to use to read SDMX data
+#'
+#' @author Emmanuel Blondel \email{emmanuel.blondel1@@gmail.com}
+#' @usage readSDMX(file, isURL)  
+#' @param file path to SDMX-ML document that needs to be parsed
+#' @param isURL a value of class "logical" either the path is an url, and data 
+#' has to be downloaded from a SDMX web-repository. Default value is TRUE.
+#' @examples 
+#' #example 1: FAO data
+#' url1 <- "http://data.fao.org/sdmx/repository/data/CROP_PRODUCTION/.156.5312../FAO?startPeriod=2008&endPeriod=2008"
+#' sdmx1 <- readSDMX(url1)
+#' stats1 <- as.data.frame(sdmx1)
+#' head(stats1)
+#' 
+#' #example 2: OECD data
+#' #url2 <- "http://stats.oecd.org/restsdmx/sdmx.ashx/GetData/MIG/TOT../OECD?startTime=2000&endTime=2011"
+#' #sdmx2 <- readSDMX(url2)
+#' #stats2 <- as.data.frame(sdmx2)
+#' #head(stats2)
+#' 
+#' #example 3: Eurostat data (local file)
+#' tmp <- system.file("data","SDMXGenericDataExample.xml", package="rsdmx")
+#' stats3 <- readSDMX(tmp, isURL = F)
+#' head(stats3) 
+
 # E.Blondel - 2013/06/10
 #=======================
 
