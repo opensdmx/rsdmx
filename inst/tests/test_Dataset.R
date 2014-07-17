8a4190c7445a62c0649c2086ed083193df7192bf
Albrecht Gradmann <agradmann@gmail.com>
2014-07-17 23:23:38 +0200

    add test for Eurostat example

--- inst/tests/test_Dataset.R
+++ inst/tests/test_Dataset.R
@@ -5,6 +5,7 @@
 #=======================
 require(rsdmx, quietly = TRUE)
 require(testthat)
+require(XML)
 context("SDMXDataSet")
 
 test_that("GenericData",{
@@ -38,4 +39,26 @@ test_that("CompactData",{
   expect_true(is.na(df[nrow(df),]$OBS_VALUE))
   expect_true(is.na(df[nrow(df)-1,]$YEA))
   expect_true(is.na(df[nrow(df)-1,]$OBS_VALUE))
+})
+
+test_that("GenericData_Eurostat",{
+  file <- system.file("data", "SDMXGenericDataExample_Eurostat.xml", package = "rsdmx")
+  xmlObj <- xmlParse(file)
+  
+  ds <- SDMXDataSet(xmlObj)
+  expect_is(ds, "SDMXDataSet")
+  
+  df <- as.data.frame(ds)
+  expect_is(df, "data.frame")
+  
+  # test df content
+  expect_equal(df, structure(list(UNIT = c("PC", "PC"), REASON = c("ECO", "ECO"), 
+                                  ENTERPR = c("OTH", "YHE"), NACE_R2 = c("B-E", "M"), 
+                                  GEO = c("PL", "PL"), FREQ = c("A", "A"), 
+                                  obsTime = structure(c(1L, 1L), .Label = "2009", 
+                                                      class = "factor"), 
+                                  obsValue = c(16.8, 18)), .Names = c("UNIT", "REASON", "ENTERPR", "NACE_R2", "GEO", "FREQ", "obsTime", "obsValue"), class = "data.frame",
+                             row.names = 1:2))
+  
+  
 })
