99e25da54550882a6aca3a468ca39527394efa8f
eblondel <emmanuel.blondel1@gmail.com>
2014-07-29 10:46:22 +0200

    refactor S4 Dataset model

--- R/Class-SDMXDataSet.R
+++ R/Class-SDMXDataSet.R
@@ -3,12 +3,12 @@
 
 #SDMX abstract class
 setClass("SDMXDataSet",
-		representation("SDMX"),
-		prototype = list(),
-		validity = function(object){
-			
-			#eventual validation rules
-			return(TRUE);
-		}
+         contains = "SDMX",
+         representation(),
+         prototype = list(),
+         validity = function(object){
+           #eventual validation rules
+           return(TRUE);
+         }
 )
 
