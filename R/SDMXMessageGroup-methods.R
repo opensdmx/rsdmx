ad003902184dea2ba0b085c54373c37d7e4f6728
eblondel <emmanuel.blondel1@gmail.com>
2014-08-06 17:06:16 +0200

    master #26 fix typos

--- R/SDMXMessageGroup-methods.R
+++ R/SDMXMessageGroup-methods.R
@@ -9,10 +9,11 @@ SDMXMessageGroup <- function(xmlObj){
 
 #methods
 as.data.frame.SDMXMessageGroup <- function(x, ...){
-  #TODO support for other included message types (at now limited to SDMXGenericData for making it work with OECD)
+  #TODO support for other included message types
+  #(at now limited to SDMXGenericData for making it work with OECD)
   dataset <- as.data.frame.SDMXGenericData(x) 
   return(dataset)
 }
 
-setAs("SDMXGenericData", "data.frame",
-      function(from) as.data.frame.SDMXMessague(from));
+setAs("SDMXMessageGroupe", "data.frame",
+      function(from) as.data.frame.SDMXMessageGroup(from));
