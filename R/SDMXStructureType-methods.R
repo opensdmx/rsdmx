e4517ff7c9b7c52bea120b67063eb786f53b15c3
eblondel <emmanuel.blondel1@gmail.com>
2014-08-20 11:38:19 +0200

    #23 fix type.SDMXStructureType function

--- R/SDMXStructureType-methods.R
+++ R/SDMXStructureType-methods.R
@@ -9,15 +9,37 @@ SDMXStructureType <- function(xmlObj){
 
 type.SDMXStructureType <- function(xmlObj){
   
+  sdmxVersion <- version.SDMXSchema(xmlObj)
+  VERSION.21 <- sdmxVersion == "2.1"
+  
   namespaces <- namespaces.SDMX(xmlObj)
   messageNs <- findNamespace(namespaces, "message")
+  strNs <- findNamespace(namespaces, "structure")
   
-  structuresXML <- getNodeSet(xmlObj, "//ns:Structures", namespaces = messageNs)
-  if(length(structuresXML) == 0){
-    return("DataStructureDefinitionsType")
+  if(VERSION.21){
+    dsXML <- getNodeSet(xmlObj, "//ns:DataStructures", namespaces = strNs)
+    ccXML <- getNodeSet(xmlObj, "//ns:Concepts", namespaces = strNs)
+    clXML <- getNodeSet(xmlObj, "//ns:CodeLists", namespaces = strNs)   
+    if(all(c(length(dsXML)>0,length(ccXML)>0,length(clXML)>0))){
+      return("DataStructureDefinitionsType")
+    }else{
+      #others
+      structuresXML <- getNodeSet(xmlObj, "//ns:Structures", namespaces = messageNs)
+      strType <- paste(xmlName(xmlChildren(structuresXML[[1]])[[1]]), "Type", sep="") 
+      return(strType)
+    }
   }else{
-    strType <- paste(xmlName(xmlChildren(structuresXML[[1]])[[1]]), "Type", sep="") 
-    return(strType)
+    dsXML <- getNodeSet(xmlObj, "//ns:KeyFamilies", namespaces = messageNs)
+    ccXML <- getNodeSet(xmlObj, "//ns:Concepts", namespaces = messageNs)
+    clXML <- getNodeSet(xmlObj, "//ns:CodeLists", namespaces = messageNs)
+    if(all(c(length(dsXML)>0, length(ccXML)>0, length(clXML)>0))){
+      #DSD
+      return("DataStructureDefinitionsType")
+    }else{
+      #others
+      if(length(ccXML)>0) return("ConceptsType")
+      if(length(clXML)>0) return("CodelistsType")
+    }
   }
   return(NULL)
 }
