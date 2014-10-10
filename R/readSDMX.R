e7f114939df688c59076c578eae32dddc5d985de
Emmanuel Blondel <emmanuel.blondel1@gmail.com>
2014-10-10 14:56:05 -0600

    formating readSDMX

--- R/readSDMX.R
+++ R/readSDMX.R
@@ -6,67 +6,64 @@
 readSDMX <- function(file, isURL = TRUE){
 	
 	#load data
-  status <- 0
+  	status <- 0
 	if(isURL == FALSE){
 		if(!file.exists(file))
 			stop("File ", file, "not found\n")
 		xmlObj <- xmlTreeParse(file, useInternalNodes = TRUE)
-    status <- 1
+    		status <- 1
 	}else{
-    rsdmxAgent <- paste("rsdmx/",as.character(packageVersion("rsdmx")),sep="")
-		content <- getURL(
-      file,
-      httpheader = list('User-Agent' = rsdmxAgent)
-    )
+    	rsdmxAgent <- paste("rsdmx/",as.character(packageVersion("rsdmx")),sep="")
+		content <- getURL(file, httpheader = list('User-Agent' = rsdmxAgent))
     
 		status <- tryCatch({
-      if(attr(regexpr("<!DOCTYPE html>", content), "match.length") == -1){
-        xmlObj <- xmlTreeParse(content, useInternalNodes = TRUE)
-        status <- 1
-      }else{
-        stop("The SDMX web-request failed. Please retry")
-      }
+      		if(attr(regexpr("<!DOCTYPE html>", content), "match.length") == -1){
+        		xmlObj <- xmlTreeParse(content, useInternalNodes = TRUE)
+        		status <- 1
+      		}else{
+        		stop("The SDMX web-request failed. Please retry")
+      		}
 		},error = function(err){
-		  print(err)
-      status <- 0
-      return(status)
+			print(err)
+			status <- 0
+			return(status)
 		})
 	}
   
-  #internal function for SDMX Structure-based document
+  	#internal function for SDMX Structure-based document
 	getSDMXStructureObject <- function(xmlObj){
-	  strTypeObj <- SDMXStructureType(xmlObj)
-	  strType <- getStructureType(strTypeObj)
-	  strObj <- switch(strType,
-	             "ConceptsType" = SDMXConcepts(xmlObj),
-	             "CodelistsType" = SDMXCodelists(xmlObj),
-               "DataStructuresType" = SDMXDataStructures(xmlObj),
-	             "DataStructureDefinitionsType" = SDMXDataStructureDefinition(xmlObj), 
-               NULL
+	strTypeObj <- SDMXStructureType(xmlObj)
+	strType <- getStructureType(strTypeObj)
+	strObj <- switch(strType,
+		"ConceptsType" = SDMXConcepts(xmlObj),
+	  	"CodelistsType" = SDMXCodelists(xmlObj),
+	  	"DataStructuresType" = SDMXDataStructures(xmlObj),
+	  	"DataStructureDefinitionsType" = SDMXDataStructureDefinition(xmlObj),
+	  	NULL
 	  )
 	  return(strObj)
 	}  
 	
 	#encapsulate in S4 object
-  obj <- NULL
-  if(status){ 
-  	type <- SDMXType(xmlObj)@type
-  	obj <- switch(type,
-                  "StructureType" = getSDMXStructureObject(xmlObj),
-                  "GenericDataType" = SDMXGenericData(xmlObj),
-                  "CompactDataType" = SDMXCompactData(xmlObj),
-                  "MessageGroupType" = SDMXMessageGroup(xmlObj),
-                  NULL
-          )	
+	obj <- NULL
+  	if(status){ 
+  		type <- SDMXType(xmlObj)@type
+  		obj <- switch(type,
+  			"StructureType" = getSDMXStructureObject(xmlObj),
+  			"GenericDataType" = SDMXGenericData(xmlObj),
+  			"CompactDataType" = SDMXCompactData(xmlObj),
+  			"MessageGroupType" = SDMXMessageGroup(xmlObj),
+  			NULL
+		)	
   
-    if(is.null(obj)){
-      if(type == "StructureType"){
-        strTypeObj <- SDMXStructureType(xmlObj)
-        type <- getStructureType(strTypeObj)
-      }
-  		stop(paste("Unsupported SDMX Type '",type,"'",sep=""))
+    	if(is.null(obj)){
+      		if(type == "StructureType"){
+        		strTypeObj <- SDMXStructureType(xmlObj)
+        		type <- getStructureType(strTypeObj)
+      		}
+  			stop(paste("Unsupported SDMX Type '",type,"'",sep=""))
+  		}
   	}
-  }
 	return(obj);
 }
 
