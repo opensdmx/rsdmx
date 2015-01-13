c680df787ba4ae419b17dddca203d6fb4b46083f
eblondel <emmanuel.blondel1@gmail.com>
2015-01-13 23:51:44 +0100

    #41 adding missing part to CompactData data.frame coercing

--- R/SDMXCompactData-methods.R
+++ R/SDMXCompactData-methods.R
@@ -51,26 +51,42 @@ as.data.frame.SDMXCompactData <- function(x, ...){
   #function to parse a Serie
   parseSerie <- function(x){
     
-    #obs values
+    #obs attributes (which may include observations)
     obsValueXML <- xmlChildren(x)
-    obsValue <- as.data.frame(
+    obsAttrs <- as.data.frame(
       do.call("rbind", lapply(obsValueXML, function(t){
         xmlAttrs(t)
       })),
       stringAsFactors = FALSE,
       row.names = 1:length(obsValueXML),
-      stringAsFactors = FALSE)
+      stringAsFactors = FALSE
+    )
+    
+    #obs children (in case we have)
+    obsValues <- try(xmlToDataFrame(obsValueXML, stringsAsFactors = FALSE), silent=TRUE)
+    if(class(obsValues) == "try-error"){
+      obsValues <- NULL
+    }else{
+      obsKeyNames <- names(lapply(obsValueXML, xmlChildren)[["Key"]])
+      obsValues[,obsKeyNames] <- obsValues[1,obsKeyNames]
+      obsValues <- obsValues[-1,]
+      invisible(lapply(obsKeyNames, function(t) obsValues[nchar(obsValues[,t])==0,t] <<- NA))
+    }
     
     #key values
     keydf <- as.data.frame(t(as.data.frame(xmlAttrs(x), stringAsFactors = FALSE)), stringAsFactors = FALSE)
-    if(nrow(obsValue) > 0){
-      keydf <- keydf[rep(row.names(keydf), nrow(obsValue)),]
-      if(class(keydf) == "data.frame") row.names(keydf) <- 1:nrow(obsValue)
+    if(nrow(obsAttrs) > 0){
+      keydf <- keydf[rep(row.names(keydf), nrow(obsAttrs)),]
+      if(class(keydf) == "data.frame") row.names(keydf) <- 1:nrow(obsAttrs)
     }
     
     #single Serie as DataFrame
-    if(nrow(obsValue) > 0){  
-      serie <- cbind(keydf, obsValue, row.names = 1:nrow(obsValue))
+    if(nrow(obsAttrs) > 0){
+      obsContent <- obsAttrs
+      if(!is.null(obsValues)){
+        obsContent <- cbind(obsAttrs, obsValues)
+      }
+      serie <- cbind(keydf, obsContent, row.names = 1:nrow(obsAttrs))
     }else{
       #manage absence data
       serie <- keydf
