a78bbbcf7cb42ba3c30ab6fc18deda59b85a5003
Emmanuel Blondel <emmanuel.blondel1@gmail.com>
2015-10-07 12:43:47 +0200

    ignore .onLoad from cov stats

--- R/profile.R
+++ R/profile.R
@@ -1,4 +1,4 @@
-.onLoad <- function (libname, pkgname) {
+.onLoad <- function (libname, pkgname) { # nocov start
   assign(".rsdmx.options", new.env(), envir= asNamespace(pkgname))
   setSDMXServiceProviders()
-}
+} # nocov end
