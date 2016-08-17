df0e23b9103ffb58f346e65a41933b5da3c35934
eblondel <emmanuel.blondel1@gmail.com>
2016-08-17 12:17:52 +0200

    preparing CRAN submission

--- R/rsdmx.R
+++ R/rsdmx.R
@@ -17,7 +17,7 @@
 #'    Type: \tab Package\cr
 #'    Version
 #'    : \tab 0.5-5\cr
-#'    Date: \tab 2016-08-02\cr
+#'    Date: \tab 2016-08-17\cr
 #'    License: \tab GPL(>=2.0)\cr
 #'    LazyLoad: \tab yes\cr
 #'  }
