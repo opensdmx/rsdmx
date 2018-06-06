34bca8be6ca33244a5a1efc54776b2bdf6fcd5c4
eblondel <emmanuel.blondel1@gmail.com>
2018-06-06 02:08:26 +0200

    fix Imports XML version number

--- R/rsdmx.R
+++ R/rsdmx.R
@@ -17,7 +17,7 @@
 #'    Type: \tab Package\cr
 #'    Version
 #'    : \tab 0.5-12\cr
-#'    Date: \tab 2018-05-28\cr
+#'    Date: \tab 2018-06-06\cr
 #'    License: \tab GPL(>=2.0)\cr
 #'    LazyLoad: \tab yes\cr
 #'  }
