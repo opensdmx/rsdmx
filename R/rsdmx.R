bee18f47d5249fd2c9208bd7d466a9072d57b74d
eblondel <emmanuel.blondel1@gmail.com>
2018-09-21 01:17:53 +0200

    change date for CRAN revision

--- R/rsdmx.R
+++ R/rsdmx.R
@@ -17,7 +17,7 @@
 #'    Type: \tab Package\cr
 #'    Version
 #'    : \tab 0.5-13\cr
-#'    Date: \tab 2018-08-22\cr
+#'    Date: \tab 2018-09-21\cr
 #'    License: \tab GPL(>=2.0)\cr
 #'    LazyLoad: \tab yes\cr
 #'  }
