9b7d426c2f2cd832ca2249a8e9bea01791672a5c
eblondel <emmanuel.blondel1@gmail.com>
2020-08-28 15:23:36 +0200

    pkg version/date

--- R/rsdmx.R
+++ R/rsdmx.R
@@ -16,8 +16,8 @@
 #'    Package: \tab rsdmx\cr
 #'    Type: \tab Package\cr
 #'    Version
-#'    : \tab 0.5-14\cr
-#'    Date: \tab 2020-04-06\cr
+#'    : \tab 0.5-15\cr
+#'    Date: \tab 2020-08-28\cr
 #'    License: \tab GPL(>=2.0)\cr
 #'    LazyLoad: \tab yes\cr
 #'  }
